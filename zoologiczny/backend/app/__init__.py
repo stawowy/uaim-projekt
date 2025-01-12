0506afda54c357aaa232ff1197673f250ea2bbbf
stawowy <marcin.stawowy4@gmail.com>
2025-01-12 18:27:02 +0100

    Another attempt to fix CORS

--- zoologiczny/backend/app/__init__.py
+++ zoologiczny/backend/app/__init__.py
@@ -12,12 +12,12 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
+    from .routes import register_blueprints
+    register_blueprints(app)
+
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
     cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
-
-    from .routes import register_blueprints
-    register_blueprints(app)
 
     return app
